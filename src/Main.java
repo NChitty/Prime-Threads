5f48d1383867a6ae0894b94ff2cd686fc1a28214
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-01-19 20:31:55 -0500

    Using division of prime numbers only

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,30 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
     public static final long MAX_NUMBER = 100000000;
     public static final int NUMBER_OF_THREADS = 8;
-    public static final long RANGE = (long) ((MAX_NUMBER-2)/8 + .5);
 
+    public static AtomicLong number = new AtomicLong(3);
     public static AtomicLong sumOfPrimes = new AtomicLong();
+
+    public static Semaphore primeList = new Semaphore(NUMBER_OF_THREADS, true);
 
     public static volatile List<Long> primes;
 
 
     public static void main(String[] args) {
         primes = new ArrayList<>();
+        primes.add(2L);
         long start = System.currentTimeMillis();
         PrimeThread[] pthreads = new PrimeThread[NUMBER_OF_THREADS];
-        long startRange = 2;
-        long endRange = 2 + RANGE;
         for(int i = 0; i < NUMBER_OF_THREADS; i++) {
-            pthreads[i] = new PrimeThread("Thread " + (i+1), startRange, endRange);
+            pthreads[i] = new PrimeThread("Thread " + (i+1));
             pthreads[i].start();
-            startRange = endRange;
-            endRange += RANGE;
         }
         for(PrimeThread t : pthreads) {
             try {
@@ -51,29 +51,40 @@ public class Main {
     static class PrimeThread extends Thread {
 
         private long number;
-        private long end;
 
-        public PrimeThread(String name, long startRange, long endRange) {
+        public PrimeThread(String name) {
             super(name);
-            number = startRange;
-            end = endRange;
+            this.number = Main.number.getAndAdd(2);
         }
 
         @Override
         public void run() {
-            for(; this.number < this.end; this.number++) {
-                long test = 2;
+            while(this.number < MAX_NUMBER) {
                 boolean prime = true;
-                while(test <= Math.ceil(Math.sqrt(this.number))) {
-                    prime = (Math.floorMod(number, test) != 0);
+                try {
+                    Main.primeList.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for(Long l : Main.primes) {
+                    if(l > Math.ceil(Math.sqrt(this.number)))
+                        break;
+                    prime = this.number % l != 0;
                     if(!prime)
                         break;
-                    test++;
                 }
+                Main.primeList.release();
                 if(prime) {
+                    try {
+                        Main.primeList.acquire(NUMBER_OF_THREADS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     Main.primes.add(this.number);
                     Main.sumOfPrimes.getAndAdd(this.number);
+                    Main.primeList.release(NUMBER_OF_THREADS);
                 }
+                this.number = Main.number.getAndAdd(2);
             }
         }
     }
