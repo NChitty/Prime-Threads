efe233ec53889fdb21a09d7ef38f86ff178dc55c
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-01-26 22:10:49 -0500

    Sieve attempt

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,129 @@
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static final long MAX_NUMBER = 100000000;
+    public static final long MAX_NUMBER = 1000000;
     public static final int NUMBER_OF_THREADS = 8;
 
-    public static AtomicLong number = new AtomicLong(3);
-    public static AtomicLong sumOfPrimes = new AtomicLong();
+    /**
+     * List of all the primes
+     */
+    public static List<Long> primes;
 
-    public static Semaphore primeList = new Semaphore(NUMBER_OF_THREADS, true);
+    public static long sumOfPrimes;
 
-    public static volatile List<Long> primes;
+    public static volatile BitSet initialSieve = new BitSet((int) Math.sqrt(MAX_NUMBER));
+    public static volatile BitSet rangeSieve = new BitSet((int) Math.sqrt(MAX_NUMBER));
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         primes = new ArrayList<>();
-        primes.add(2L);
+
+        initialSieve.set(0);
+        initialSieve.set(1);
+        initialSieve.set(4);
+        initialSieve.set(6);
+        initialSieve.set(8);
+        initialSieve.set(9);
+        initialSieve.set(10);
+        initialSieve.set(12);
+        initialSieve.set(14);
+        initialSieve.set(15);
+        initialSieve.set(16);
+        initialSieve.set(18);
+        initialSieve.set(20);
+        initialSieve.set(21);
+        rangeSieve.set(0);
+        rangeSieve.set(1);
+        rangeSieve.set(4);
+        rangeSieve.set(6);
+        rangeSieve.set(8);
+        rangeSieve.set(9);
+        rangeSieve.set(10);
+        rangeSieve.set(12);
+        rangeSieve.set(14);
+        rangeSieve.set(15);
+        rangeSieve.set(16);
+        rangeSieve.set(18);
+        rangeSieve.set(20);
+        rangeSieve.set(21);
+
         long start = System.currentTimeMillis();
-        PrimeThread[] pthreads = new PrimeThread[NUMBER_OF_THREADS];
-        for(int i = 0; i < NUMBER_OF_THREADS; i++) {
-            pthreads[i] = new PrimeThread("Thread " + (i+1));
-            pthreads[i].start();
-        }
-        for(PrimeThread t : pthreads) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        long prime;
+        long startRange = 0;
+        long endRange = (long) Math.sqrt(MAX_NUMBER);
+        for(int i = 0; i < Math.sqrt(MAX_NUMBER); i++) {
+            int currentIndex = 0;
+            int rangeIndex = 0;
+            Thread[] threads = new Thread[NUMBER_OF_THREADS];
+            while(currentIndex < Math.sqrt(endRange)) {
+                for(int j = 0; j < NUMBER_OF_THREADS; j++) {
+                    currentIndex = initialSieve.nextClearBit(currentIndex+1);
+                    if(currentIndex > Math.sqrt(endRange)) break;
+                    threads[j] = new Thread(new SieveRunnable(currentIndex, startRange, endRange));
+                    threads[j].start();
+                }
+                for(int j = 0; j < NUMBER_OF_THREADS; j++) {
+                    if(threads[j] != null)
+                        threads[j].join();
+                }
             }
+            while(rangeIndex < endRange-startRange) {
+                rangeIndex = rangeSieve.nextClearBit(rangeIndex);
+                prime = rangeSieve.nextClearBit(rangeIndex) + startRange;
+                if(prime < endRange) {
+                    primes.add(prime);
+                    sumOfPrimes += prime;
+                } else
+                    break;
+                rangeIndex++;
+            }
+            rangeSieve.clear();
+            startRange = endRange;
+            endRange += Math.sqrt(MAX_NUMBER);
         }
         long end = System.currentTimeMillis();
+
         int noOfPrimes = primes.size();
         System.out.printf(
-                "%fs\t%s\t%s\n",
-                (end - start)/1000.0,
+                "%dms\t%s\t%s\n",
+                (end - start),
                 DecimalFormat.getInstance().format(noOfPrimes),
-                DecimalFormat.getInstance().format(sumOfPrimes.get())
+                DecimalFormat.getInstance().format(sumOfPrimes)
         );
+
         primes.sort(Collections.reverseOrder());
         for(int i = 9; i >= 0; i--) {
             System.out.println(primes.get(i));
         }
     }
 
-    static class PrimeThread extends Thread {
+    static class SieveRunnable implements Runnable {
 
-        private long number;
+        private long base;
+        private long start;
+        private long end;
 
-        public PrimeThread(String name) {
-            super(name);
-            this.number = Main.number.getAndAdd(2);
+        public SieveRunnable(int currentIndex, long rangeStart, long rangeEnd) {
+            this.base = currentIndex;
+            this.start = rangeStart;
+            this.end = rangeEnd;
         }
 
         @Override
         public void run() {
-            while(this.number < MAX_NUMBER) {
-                boolean prime = true;
-                try {
-                    Main.primeList.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                for(Long l : Main.primes) {
-                    if(l > Math.ceil(Math.sqrt(this.number)))
-                        break;
-                    prime = this.number % l != 0;
-                    if(!prime)
-                        break;
-                }
-                Main.primeList.release();
-                if(prime) {
-                    try {
-                        Main.primeList.acquire(NUMBER_OF_THREADS);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Main.primes.add(this.number);
-                    Main.sumOfPrimes.getAndAdd(this.number);
-                    Main.primeList.release(NUMBER_OF_THREADS);
-                }
-                this.number = Main.number.getAndAdd(2);
+            // Gives the lowest multiple of base in the range
+            long multiple = this.start == 0 ? this.base + this.base :
+                    (this.start % this.base) == 0 ? this.start : (this.start - (this.start%this.base) + this.base);
+            while(multiple <= end) {
+                if(this.start == 0)
+                    initialSieve.set((int) multiple, true);
+                rangeSieve.set((int) (multiple - start), true);
+                multiple += this.base;
             }
         }
     }
+
 
 }
